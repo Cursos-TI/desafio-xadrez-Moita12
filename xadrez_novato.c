43aba2f34ca3d1c5a23a38580367ebcf8fcf9b8c
Moisés Camargo Da Silva <133894628+Moita12@users.noreply.github.com>
2025-06-12 09:20:32 -0300

    Update xadrez_novato.c

--- xadrez_novato.c
+++ xadrez_novato.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    // Simulação do movimento da Torre (5 casas para a direita)
+    
     int torreMovimentos = 5;
     printf("Movimento da Torre:\n");
     for (int i = 0; i < torreMovimentos; i++) {
         printf("Direita\n");
     }
 
-    // Simulação do movimento do Bispo (5 casas na diagonal para cima e à direita)
+    
     int bispoMovimentos = 5;
     int contadorBispo = 0;
     printf("\nMovimento do Bispo:\n");
@@ -17,7 +17,7 @@ int main() {
         contadorBispo++;
     }
 
-    // Simulação do movimento da Rainha (8 casas para a esquerda)
+ 
     int rainhaMovimentos = 8;
     int contadorRainha = 0;
     printf("\nMovimento da Rainha:\n");
